675f1bdcee594d12c1b43ec6559454df8bca4150
Ciarán Crowley <ciarancrowley37@gmail.com>
2023-07-19 11:03:13 +0100

    git ignore

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,12 +3,32 @@ using System;
 
 namespace HelloWorld // Note: actual namespace depends on the project name.
 {
+
+  public class Computer
+  {
+    // private string _motherboard;
+    public string Motherboard { get; set; } = "";
+    public int CPUCores { get; set; }
+    public bool hasWifi { get; set; }
+    public bool hasLTE { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public decimal Price { get; set; }
+    public string VideoCard { get; set; } = "";
+  }
   internal class Program
   {
-
-
     static void Main(string[] args)
     {
+      Computer myComputer = new Computer()
+      {
+        Motherboard = "Z690",
+        hasWifi = true,
+        hasLTE = true,
+        ReleaseDate = DateTime.Now,
+        Price = 943.87m,
+        VideoCard = "RTX 2060"
+      };
+      Console.WriteLine(myComputer.Motherboard);
 
     }
 
