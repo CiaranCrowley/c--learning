1689323f7da482565c670c2b42a377ef37d8c790
Ciarán Crowley <ciarancrowley37@gmail.com>
2023-07-24 11:39:49 +0100

    Installed some Packages and established connection with localhost DB

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Data;
+using Dapper;
 using HelloWorld.Models;
+using Microsoft.Data.SqlClient;
 
 namespace HelloWorld // Note: actual namespace depends on the project name.
 {
@@ -10,6 +13,17 @@ namespace HelloWorld // Note: actual namespace depends on the project name.
   {
     static void Main(string[] args)
     {
+
+      string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;";
+
+      IDbConnection dbConnection = new SqlConnection(connectionString);
+
+      string sqlCommand = "SELECT GETDATE()";
+
+      DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+
+      Console.Write(rightNow);
+
       Computer myComputer = new Computer()
       {
         Motherboard = "Z690",
